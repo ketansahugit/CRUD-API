1523ed7c2f4a8c87f8029956c1bd8e51b913b5db
Ketan Sahu <rorozoro9888@gmail.com>
2023-08-28 22:34:36 +0530

    API complete

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/Ketan",
 })
 .catch((err) => {
     console.log(err);
-})
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.json())
+app.use(express.json());
 
 const productSchema = new mongoose.Schema({
     name: String,
     description: String,
     price: Number,
-})
+});
 
 const Product = new mongoose.model("Product", productSchema);
 
@@ -33,10 +33,57 @@ app.post("/api/v1/product/new", async(req, res) => {
     res.status(200).json({
         success:true,
         product
-    })
-})
+    });
+});
 
+// Read Product
+app.get("/api/v1/products", async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true, products
+    });
+});
+
+// Update Product
+app.put("/api/v1/product/:id", async(req, res) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found!"
+        });
+    };
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, 
+        useFindAndModify: true, 
+        runValidators: true
+    });
+    res.status(200).json({
+        success: true,
+        product
+    })
+});
+
+// Delete Product
+app.delete("/api/v1/product/:id", async(req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found!"
+        });
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product is deleted successfully"
+    });
+})
 
 app.listen(4500, () => {
     console.log('server is working http://localhost:4500')
-})
+});
